e4cd2b0b9643245af2fc2a32ab63929158822e85
fitaliano@gmail.com <fitaliano@gmail.com@13d339e1-fc1d-3643-1fc2-b70da922f878>
2014-06-12 03:36:04 +0000

    se agrega: -------------------------------- Roles  (Models/Roles.cs) Flujos  (Models/Flujos.cs   Models/FlujosDAO.cs) bootstrap responsive menu ( Views/Shared/_Layout.cshtml )

---
+++ MvcApplication1/Models/Flujos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Flujos
+    {
+        private int rol_id;
+        private int estado_inicial_id;
+        private int estado_final_id;
+        private int flujo_habilitado;
+
+        public int Rol_id
+        {
+            get { return this.rol_id; }
+            set { this.rol_id = value; }
+        }
+
+        public int Estado_inicial_id
+        {
+            get { return this.estado_inicial_id; }
+            set { this.estado_inicial_id = value; }
+        }
+
+        public int Estado_final_id
+        {
+            get { return this.estado_final_id; }
+            set { this.estado_final_id = value; }
+        }
+
+        public int Flujo_habilitado
+        {
+            get { return this.flujo_habilitado; }
+            set { this.flujo_habilitado = value; }
+        }
+    }
+}
---
+++ MvcApplication1/Models/FlujosDAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MvcApplication1.Models
+{
+    public class FlujosDAO
+    {
+
+        private List<int> flujoPermitido = new List<int>();
+
+        public List<int> getFlujoPermitido(int rol_id, int estado_inicial_id)
+        {
+            Datos data = new Datos(); 
+            data.conectar();
+            SqlCommand command = data.sqlConnection.CreateCommand();
+            command.CommandText = "Select * from FLOW where rol_id = " + rol_id + " and estado_inicial = " + estado_inicial_id ;
+            command.CommandType = CommandType.Text;
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                flujoPermitido.Add((int)reader["ESTADO_FINAL_ID"]);
+            }
+            reader.Close();
+            data.sqlConnection.Close();
+            return flujoPermitido;
+        }
+
+    }
+}
